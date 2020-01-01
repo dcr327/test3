d45f85bbc10111a0954ac738e5c72180960b7f91
tom_glb <goodMorning_glb@atguigu.com>
2020-01-02 01:03:39 +0800

    commit ha

--- src/main/java/com/atguigu/git/Happy.java
+++ src/main/java/com/atguigu/git/Happy.java
@@ -4,6 +4,8 @@ public class Happy {
 	public static void main(String[] args) {
 		System.out.println("right....");
 		System.out.println("342134");
+		System.out.println("342134");
+		System.out.println("ttt");
 		
 	}
 
